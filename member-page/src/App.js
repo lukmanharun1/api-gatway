37187041774e25a77d94b9cb78ca03aae27915af
lukman <nandes88.ni@gmail.com>
2021-01-08 18:00:33 +0700

    membuat project member-page

---
+++ member-page/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import 'assets/css/style.css';
+function App() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-6x1">
+        Javascript React
+      </h1>
+    </div>
+  );
+}
+
+export default App;
